6adce0ca9fdf05b47db424c6f824ad049c300ab0
KaliMasterDev <KaliMasterDev@users.github.com>
2020-08-30 19:57:03 +0200

    Add some functions
    Add clock, day and night, FPS meter, trees, roses and wild berries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, noise
+import pygame, noise, colorsys
 from pypresence import Presence
 from time import time, perf_counter
 from random import choice
@@ -95,7 +95,7 @@ def mainMenu():
                         buttons_active_settings = [False, False]
         # endregion
         # region draw_menu
-        font = pygame.font.SysFont("Consolas", 42)
+        font = pygame.font.Font("Consolas.ttf", 42)
         if not in_settings:
             if in_settings_before:
                 menu_bg = pygame.image.load("MainMenu.png")
@@ -160,13 +160,18 @@ def mainMenu():
 
 class materials:
     air = 0
-    grass = 1
+    grass_block = 1
     dirt = 2
     stone = 3
     coal = 4
     iron = 5
     gold = 6
     diamond = 7
+    trunk = 8
+    leaves = 9
+    grass = 10
+    rose = 11
+    berry_bush = 12
 
 def generateWorld():
     try:
@@ -178,11 +183,12 @@ def generateWorld():
     pygame.display.flip()
 
     global world
-    world = [[materials.air for col in range(600)] for row in range(5000)]
+    world = [[materials.air for row in range(600)] for col in range(5000)]
     maxG = 500
     minG = 100
     maxGS = 5+3
-    offset_x = choice(range(850000))
+    offsetG = choice(range(850000))
+    offsetS = choice(range(850000))
 
     for i in range(len(world)):
         if i % 10 == 0:
@@ -190,8 +196,8 @@ def generateWorld():
                 if event.type == pygame.QUIT:
                     exit()
 
-        heightG = round((noise.pnoise1((i + offset_x) / 700, 5, 0.5, 2, 900000)+1)*(maxG-minG)/3) + minG
-        heightS = heightG - 3 - round((noise.pnoise1((i + offset_x) / 700, 1, 0.5, 2, 900000) + 2) * maxGS / 1.5)
+        heightG = round((noise.pnoise1((i + offsetG) / 900, 5, 0.5, 2, 900000)+1)*(maxG-minG)/3) + minG
+        heightS = heightG - 3 - round((noise.pnoise1((i + offsetS) / 600, 1, 0.5, 2, 900000) + 2) * maxGS / 1.5)
 
         # print(heightG, ' ', heightS)
 
@@ -236,13 +242,124 @@ def generateWorld():
 
         for j in range(heightS, heightG):
             world[i][j] = materials.dirt
-        world[i][heightG] = materials.grass
+        world[i][heightG] = materials.grass_block
+        if choice(range(1, 60)) == 59 and world[i - 1][heightG] == materials.grass_block and world[i - 1][heightG + 2] != materials.trunk and i > 2 and i < 4997:
+            increment = 1
+            treetemp = choice(range(4, 7))
+            while increment <= treetemp:
+                if increment == treetemp:
+                    world[i][heightG + increment - 1] = materials.leaves
+                    world[i][heightG + increment] = materials.leaves
+                    world[i][heightG + increment + 1] = materials.leaves
+                    world[i][heightG + increment + 2] = materials.leaves
+                    world[i - 1][heightG + increment + 1] = materials.leaves
+                    world[i + 1][heightG + increment + 1] = materials.leaves
+                    world[i - 1][heightG + increment] = materials.leaves
+                    world[i - 2][heightG + increment] = materials.leaves
+                    world[i + 1][heightG + increment] = materials.leaves
+                    world[i + 2][heightG + increment] = materials.leaves
+                    world[i - 1][heightG + increment - 1] = materials.leaves
+                    world[i - 2][heightG + increment - 1] = materials.leaves
+                    world[i + 1][heightG + increment - 1] = materials.leaves
+                    world[i + 2][heightG + increment - 1] = materials.leaves
+                else:
+                    world[i][heightG + increment] = materials.trunk
+                increment += 1
+        elif world[i - 1][heightG + 1] == materials.grass and choice(range(1, 3)) == 2:
+            world[i][heightG + 1] = materials.grass
+        elif choice(range(1, 25)) == 24:
+            world[i][heightG + 1] = materials.grass
+        elif world[i-1][heightG + 1] == materials.rose and choice(range(1, 4)) == 3:
+            world[i][heightG + 1] = materials.grass
+        elif choice(range(1, 15)) == 14 and world[i - 1][heightG + 1] == materials.grass:
+            world[i][heightG + 1] = materials.rose
+        elif choice(range(1, 120)) == 119:
+            world[i][heightG + 1] = materials.rose
+        elif world[i - 1][heightG + 1] == materials.rose and world[i - 2][heightG + 1] == materials.air:
+            world[i][heightG + 1] = materials.grass
+        if world[i - 1][heightG + 1] == materials.trunk and choice(range(1, 10)) == 9:
+            world[i][heightG + 1] = materials.berry_bush
+        if world[i - 1][heightG + 1] == materials.trunk and choice(range(1, 10)) == 9 and world[i - 1][heightG] == materials.grass_block:
+            world[i - 2][heightG + 1] = materials.berry_bush
 
     # print(world)
 
+def pausemenu():
+    try:
+        rpc.update(large_image="large", details="In pause menu...", start=time())
+    except:
+        pass
+    ssrect = pygame.Rect(0, 0, 1280, 720)
+    ss = pygame.Surface((1280, 720))
+    ss.blit(screen, ssrect)
+    inPauseMenu = True
+    while inPauseMenu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                inPauseMenu = False
+        
+        screen.fill((0, 0, 0))
+        screen.blit(ss, [0, 0])
+        darkglass = pygame.Surface((1280, 720))
+        darkglass.set_alpha(200)
+        darkglass.fill((0, 0, 0))
+        screen.blit(darkglass, [0, 0])
+        pygame.display.flip()
+        clock.tick(30)
+
+
+font = pygame.font.Font("Consolas.ttf", 30)
+def updateFPSmeter():
+    global FPStext
+    global timeClockMins
+    hours = int(timeClockMins / 60)
+    mins = int(timeClockMins % 60)
+    hoursstring = str(hours)
+    if len(hoursstring) == 1:
+        hoursstring = "0" + hoursstring
+    minsstring = str(mins)
+    if len(minsstring) == 1:
+        minsstring = "0" + minsstring
+    
+    timestring = hoursstring + ":" + minsstring
+    
+    FPStext = font.render(timestring + "; " + str(int(clock.get_fps()))+"FPS", 1, (255, 0, 165))
+
+skyV = 100
+skyMode = 0
+timeClockMins = 12 * 60
+def skyColor(tempV):
+    global skyV
+    global timeClockMins
+    if skyMode == 1:
+        skyV = tempV + 0.0025
+    else:
+        skyV = tempV - 0.0025
+
+    timeClockMins += 0.018
+    
+    if skyV < 0:
+        tempV = 0
+    if skyV > 100:
+        tempV = 100
+
+    if int(timeClockMins / 60) == 24:
+        timeClockMins = 0
+
+    if tempV == 0:
+        print("MIDNIGHT", int(timeClockMins / 60), ":", int(timeClockMins % 60))
+    elif tempV == 100:
+        print("NOON", int(timeClockMins / 60), ":", int(timeClockMins % 60))
+
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(202 / 360, 0.82, tempV / 100))
 
 mainMenu()
 generateWorld()
+
+global clock
+clock = pygame.time.Clock()
 
 try:
     rpc.update(large_image="large", details="Playing a game", start=time())
@@ -250,29 +367,47 @@ except:
     pass
 
 textures = {
-    materials.grass: pygame.image.load("textures/grass.png").convert(),
+    materials.grass_block: pygame.image.load("textures/grass_block.png").convert(),
     materials.dirt: pygame.image.load("textures/dirt.png").convert(),
     materials.stone: pygame.image.load("textures/stone.png").convert(),
     materials.diamond: pygame.image.load("textures/diamond.png").convert(),
     materials.gold: pygame.image.load("textures/gold.png").convert(),
     materials.coal: pygame.image.load("textures/coal.png").convert(),
-    materials.iron: pygame.image.load("textures/iron.png").convert()
+    materials.iron: pygame.image.load("textures/iron.png").convert(),
+    materials.trunk: pygame.image.load("textures/trunk.png").convert(),
+    materials.leaves: pygame.image.load("textures/leaves.png").convert(),
+    materials.grass: pygame.image.load("textures/grass.png"),
+    materials.rose: pygame.image.load("textures/rose.png"),
+    materials.berry_bush: pygame.image.load("textures/berry_bush.png")
 }
 pygame.event.set_allowed([pygame.QUIT, pygame.MOUSEBUTTONUP, pygame.KEYDOWN, pygame.KEYUP])
 
-wx = 10
-wy = 350
 texH = 48
 texW = 48
+wx = choice(range(2000, 3000))
+temph = 599
+skyColorTuple = skyColor(skyV)
+while world[wx + int((1280 / texW) / 2)][temph] != materials.grass_block:
+    temph -= 1
+    
+wy = 600 - temph - int(texH / 720) - 12
 clock = pygame.time.Clock()
 time_move = perf_counter()
 time_events = perf_counter()
-
+updateFPSmeter()
 while True:
+    if skyV <= 0 and skyMode == 0:
+        skyMode = 1
+    elif skyV >= 100 and skyMode == 1:
+        skyMode = 0
+    
     if perf_counter() - time_events >= 2:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pausemenu()
+        skyColorTuple = skyColor(skyV)
 
     if perf_counter() - time_move >= 0.05:
         time_move = perf_counter()
@@ -293,11 +428,16 @@ while True:
         if keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_d]:
             if wx < 4999 - int(1280 / texW):
                 wx += 1
+        updateFPSmeter()
+    
 
-    screen.fill((55, 155, 250))
+    screen.fill(skyColorTuple)
 
     for y in range(int(720 / texH) + 1):
         for x in range(int(1280 / texW)+1):
             if world[wx+x][599-wy-y] != materials.air:
                 screen.blit(textures[world[wx+x][599-wy-y]], [x*texW, y*texH])
+    
+    screen.blit(FPStext, [0, 0])
     pygame.display.flip()
+    clock.tick()
